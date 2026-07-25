006dcd2069baf552d16e33d099f65bbc88b51b9a
kamaudev1 <jk7771313@gmail.com>
2026-07-25 11:06:40 +0300

    Update supabase-config.js

--- js/supabase-config.js
+++ js/supabase-config.js
@@ -2,7 +2,7 @@
 const SUPABASE_URL = 'https://dytbmobtxflxnfqkddrk.supabase.co';
 const SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImR5dGJtb2J0eGZseG5mcWtkZHJrIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODQ5MzI4MjEsImV4cCI6MjEwMDUwODgyMX0.iFwsE4a8pM_xHP37eDogTjVWkzi7NwIkzG1m8UkpCfQ';
 
-// Initialize Supabase client with proper settings for storage
+// Initialize Supabase client with proper settings
 const supabaseClient = supabase.createClient(SUPABASE_URL, SUPABASE_KEY, {
     auth: {
         autoRefreshToken: true,
