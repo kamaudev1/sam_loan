da885ea85366061eb0d6a6870ef141df4ecc0268
kamaudev1 <jk7771313@gmail.com>
2026-07-25 10:01:46 +0300

    Create supabase-config.js

---
+++ js/supabase-config.js
@@ -0,0 +1,7 @@
+// js/supabase-config.js
+const SUPABASE_URL = 'https://dytbmobtxflxnfqkddrk.supabase.co';
+const SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImR5dGJtb2J0eGZseG5mcWtkZHJrIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODQ5MzI4MjEsImV4cCI6MjEwMDUwODgyMX0.iFwsE4a8pM_xHP37eDogTjVWkzi7NwIkzG1m8UkpCfQ';
+
+// Initialize Supabase client
+const supabaseClient = supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
+
